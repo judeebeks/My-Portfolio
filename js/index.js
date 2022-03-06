20dcf4f847acd136fa8f77c42cbcd1674aca0971
Jude Ebeke <judeebekes67@gmail.com>
2022-03-06 01:03:17 +0100

    ninth commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 const presentYear = document.querySelector("#year");
 const NewDate = new Date();
+// const form = document.querySelector("form");
+// const input = document.querySelector("textarea");
+// const message = document.querySelector("#errormessage");
 
 const updateYear = (yearUpdate, year) => {
     year.innerHTML = yearUpdate.getFullYear();
 };
 
 updateYear(NewDate, presentYear);
+
+// form.addEventListener("submit", (e) => {
+//     let errorMessage = [];
+
+//     if (input.value.trim().length < 1) {
+//         errorMessage.push("Enter a text!");
+//     }
+
+//     if (errorMessage.length > 0) {
+//         e.preventDefault();
+//         message.innerHTML = errorMessage.toString();
+//     }
+// });
